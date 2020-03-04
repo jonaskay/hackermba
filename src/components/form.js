2679ed7129fde3b28fe012234276e678f4603c90
jonaskay <jonaskay@iki.fi>
2020-03-04 20:31:38 +0200

    Add working form for pre-launch list

--- src/components/form.js
+++ src/components/form.js
@@ -1,18 +1,62 @@
 import React from "react"
 
 const Form = () => (
-  <div className="inline-block text-left lg:ml-6">
-    <label className="inline-block mb-1">Email</label>
-    <br />
-    <input
-      className="border border-teal-8 rounded px-2 py-1 mr-2 mb-2 md:mb-0 w-full md:w-auto"
-      type="email"
-    />
-    <div className="inline-block text-center md:text-left w-full md:w-auto">
-      <button className="shadow-xs border border-teal-7 bg-teal-7 text-gray-0 px-2 py-1 rounded">
-        Notify me!
-      </button>
-    </div>
+  <div id="mc_embed_signup" className="inline-block text-left lg:ml-6">
+    <form
+      form
+      action="https://hackermba.us19.list-manage.com/subscribe/post?u=b38920bd69f42a9816e14ddce&amp;id=0e55a064bd"
+      method="post"
+      id="mc-embedded-subscribe-form"
+      name="mc-embedded-subscribe-form"
+      className="validate inline-block"
+      target="_blank"
+      novalidate
+    >
+      <div id="mc_embed_signup_scroll" className="inline-block">
+        <div className="mc-field-group inline-block">
+          <label htmlFor="mce-EMAIL" className="inline-block mb-1">
+            Email
+          </label>
+          <br />
+          <input
+            type="email"
+            value=""
+            name="EMAIL"
+            className="required email border border-teal-8 rounded px-2 py-1 mr-2 mb-2 md:mb-0 w-full md:w-auto"
+            id="mce-EMAIL"
+          />
+        </div>
+        <div id="mce-responses" className="clear inline-block">
+          <div
+            className="response inline-block"
+            id="mce-error-response"
+            style={{ display: "none" }}
+          ></div>
+          <div
+            className="response inline-block"
+            id="mce-success-response"
+            style={{ display: "none" }}
+          ></div>
+        </div>
+        <div style={{ left: -5000, position: "absolute" }} aria-hidden="true">
+          <input
+            type="text"
+            name="b_b38920bd69f42a9816e14ddce_0e55a064bd"
+            tabindex="-1"
+            value=""
+          />
+        </div>
+        <div className="clear inline-block text-center md:text-left w-full md:w-auto">
+          <input
+            type="submit"
+            value="Notify me!"
+            name="subscribe"
+            id="mc-embedded-subscribe"
+            className="button cursor-pointer shadow-xs border border-teal-7 bg-teal-7 text-gray-0 px-2 py-1 rounded"
+          />
+        </div>
+      </div>
+    </form>
   </div>
 )
 
