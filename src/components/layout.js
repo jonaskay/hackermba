86fe11d8ba02c67ced5473bd5d5c9df86f11a7e3
jonaskay <jonaskay@iki.fi>
2020-03-04 14:52:25 +0200

    Style desktop version of homepage

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,6 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -24,23 +23,9 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
+    <div className="container mx-auto px-4 sm:px-8">
+      <main className="max-w-4xl mx-auto">{children}</main>
+    </div>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -8,13 +7,45 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <header className="py-8">
+      <div className="shadow-md w-full max-w-xs inline-block align-top">
+        <Image />
+      </div>
+      <div className="max-w-lg ml-16 mt-12 inline-block align-top">
+        <h2 className="text-3xl font-semibold leading-tight mb-6">
+          New podcast for people who want to broaden their skills in the world
+          of software
+        </h2>
+        <p className="text-xl mb-4">
+          These are the real-life lessons about building and managing software
+          you won't learn at university:
+        </p>
+        <ul className="text-xl list-disc mb-8">
+          <li className="ml-6 my-2">
+            Interviews with authors, professionals, and entrepreneurs
+          </li>
+          <li className="ml-6 my-2">
+            Insights into the development, design, and business aspects of
+            working with digital products
+          </li>
+        </ul>
+        <p className="text-lg italic leading-relaxed mb-4">
+          HackerMBA is work-in-progress. You can get notified when the first
+          episode is out by subscribing to our pre-launch email list below.
+        </p>
+        <div className="ml-6">
+          <label className="inline-block mb-1">Email</label>
+          <br />
+          <input
+            className="border border-teal-8 rounded px-2 py-1 mr-2"
+            type="email"
+          />
+          <button className="shadow-xs border border-teal-7 bg-teal-7 text-gray-0 px-2 py-1 rounded">
+            Notify me!
+          </button>
+        </div>
+      </div>
+    </header>
   </Layout>
 )
 
